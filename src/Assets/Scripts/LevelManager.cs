0332cc271c2b996e3afd32ab3d2f7e1c9ca16f67
brook-hatia <brooktesfahun@gmail.com>
2023-03-07 20:06:23 -0600

    level load, restart and next

--- src/Assets/Scripts/LevelManager.cs
+++ src/Assets/Scripts/LevelManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isMenu = false; //check if scene is main menu
+    private int level; //level number
+
     void Start()
     {
-        
+        //check if level number has been saved and go there
+        if (PlayerPrefs.HasKey("currentLevel"))
+        {
+            level = PlayerPrefs.GetInt("currentLevel");
+        }
+
+        //if on main menu jump to saved level
+        if (isMenu)
+        {
+            LoadLevel();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //jump to saved level number
+    public void LoadLevel()
     {
-        
+        SceneManager.LoadScene(level);
+    }
+
+    //increase level number, save level number and go to next level
+    public void NextLevel()
+    {
+        level += 1;
+        PlayerPrefs.SetInt("currentLevel", level);
+        LoadLevel();
+    }
+
+    //restart current level
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
